3d20ab39cc7a8eddcf48fe18b512a8dd05414dbb
kumimasaki <masaki.kmkm@gmail>
2025-05-07 13:22:22 +0900

    register

--- STS/Hello/src/main/java/hello/com/LoginController.java
+++ STS/Hello/src/main/java/hello/com/LoginController.java
@@ -28,36 +28,44 @@ public class LoginController {
 	public String getResultPage() {
 		return "success.html";
 	}
-	
+
 	// ログイン画面から送信されたデータを受け取る
 	@PostMapping("/login/process")
 	public String login(@RequestParam String email, @RequestParam String password) {
 		// 確認用
 		System.out.println(email);
 		System.out.println(password);
-		
+
 		// もし、emailの内容が「admin@test.com」
 		// かつ、ｐasswordの内容が「admin」と等しい場合は/resultに転送（リダイレクト）
 		// そうではない場合、ログイン画面/loginに転送（リダイレクト）
-		if(email.equals("admin@test.com") && password.equals("admin")) {
+		if (email.equals("admin@test.com") && password.equals("admin")) {
 			return "redirect:/result";
 		} else {
 			return "redirect:/login";
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	/**
+	 * htmlからデータを受け取るためのURL「/register/process」 メソッド名：register
+	 * 
+	 * もし、usernameの内容が「user」 かつ、emailの内容が「admin@test.com」
+	 * かつ、passwordの内容が「admin」の場合、/resultに転送してください そうでない場合、/registerに転送してください
+	 */
+	@PostMapping("/register/process")
+	public String register(@RequestParam String username, @RequestParam String email, @RequestParam String password) {
+		if(username.equals("user") && email.equals("admin@test.com")
+				&& password.equals("admin")) {
+			return "redirect:/result";
+		} else {
+			return "redirect:/register";
+		}
+	}
+
+	
+	
+	
+	
+	
+	
 }
