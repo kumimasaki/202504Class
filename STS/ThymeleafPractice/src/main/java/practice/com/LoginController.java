ebb39b6c752040f42fc69579fc24cf798aeed401
kumimasaki <masaki.kmkm@gmail>
2025-05-07 14:32:09 +0900

    thymeleaf2

--- STS/ThymeleafPractice/src/main/java/practice/com/LoginController.java
+++ STS/ThymeleafPractice/src/main/java/practice/com/LoginController.java
@@ -1,7 +1,10 @@
 package practice.com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -9,5 +12,13 @@ public class LoginController {
 	@GetMapping("/login")
 	public String getLoginPage() {
 		return "login.html";
+	}
+	
+	@PostMapping("/login/process")
+	public String login(@RequestParam String email,
+			@RequestParam String password, Model model) {
+		model.addAttribute("userEmail", email);
+		model.addAttribute("userPassword", password);
+		return "success.html";
 	}
 }
